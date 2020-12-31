c1b15045cec8f3fbe1ac21ca81b8860acfcc7792
han <igoman2@ajou.ac.kr>
2020-12-31 16:59:21 +0900

    App release

---
+++ app/src/main/java/com/Han/HanNewYear/SoundManager.java
@@ -0,0 +1,30 @@
+package com.Han.HanNewYear;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import java.util.HashMap;
+
+public class SoundManager{
+    private SoundPool mSoundPool;
+    private HashMap<Integer, Integer> mSoundPoolMap;
+    private AudioManager mAudioManager;
+    private Context mContext;
+
+    public SoundManager(Context mContext,SoundPool mSoundPool) {
+        this.mContext = mContext;
+        this.mSoundPool = mSoundPool;
+        mSoundPoolMap = new HashMap<Integer, Integer>();
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+    }
+
+    public void addSount(int index, int soundId){ //효과음 추가
+        mSoundPoolMap.put(index,mSoundPool.load(mContext,soundId,1));
+    }
+
+    public int playSound(int index){ //효과음 재생
+        int streamVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        return mSoundPool.play(mSoundPoolMap.get(index),streamVolume,streamVolume,1,0,1f);
+    }
+
+}
